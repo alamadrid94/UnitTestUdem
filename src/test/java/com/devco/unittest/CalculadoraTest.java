2805cf54916f70e12b0cf63c73ddea98f56c411f
Angie Lamadrid <anngielc@gmail.com>
2019-11-22 16:13:00 -0500

    Unit test first class project

---
+++ src/test/java/com/devco/unittest/CalculadoraTest.java
@@ -0,0 +1,57 @@
+package com.devco.unittest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CalculadoraTest {
+    Calculadora calculadora;
+
+    @BeforeEach
+    public void setup() {
+        calculadora = new Calculadora();
+        calculadora = Mockito.spy(calculadora);
+    }
+
+    @Test
+    public void cuandoIngresa5EntoncesRetornaV() {
+        Mockito.when(calculadora.construirRomano(Mockito.anyInt())).thenReturn("V");
+
+        String respuesta = calculadora.calcular(5);
+        assertThat(respuesta, equalTo("V"));
+    }
+
+    @Test
+    public void cuandoIngresa1EntoncesRetornaI() {
+        String respuesta = calculadora.calcular(1);
+        assertThat(respuesta, equalTo("I"));
+    }
+
+    @Test
+    public void cuandoIngresa10EntoncesRetornaX() {
+        String respuesta = calculadora.calcular(10);
+        assertThat(respuesta, equalTo("X"));
+    }
+
+    @Test
+    public void cuandoIngresa3EntoncesRetornaIII() {
+        String respuesta = calculadora.calcular(3);
+        assertThat(respuesta, equalTo("III"));
+    }
+
+    @Test
+    public void cuandoIngresa4EntoncesRetornaIV() {
+        String respuesta = calculadora.calcular(4);
+        assertThat(respuesta, equalTo("IV"));
+    }
+
+    @Test
+    public void cuandoIngresa9EntoncesRetornaIX() {
+        String respuesta = calculadora.calcular(9);
+        assertThat(respuesta, equalTo("IX"));
+    }
+
+}
